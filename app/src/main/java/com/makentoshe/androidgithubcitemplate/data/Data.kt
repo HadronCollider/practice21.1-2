09f983a58e0804cbc508b4bcaf02bfabf09636ca
Vlad <andreevvlad443@gmail.com>
2021-06-07 04:18:39 +0300

    Data model classes added

---
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/data/Data.kt
@@ -0,0 +1,7 @@
+package com.makentoshe.androidgithubcitemplate.data
+
+data class Data(
+    val categories: List<Any>? = null,
+    val groups: List<Any>? = null,
+    val things: List<Thing>? = null
+)
---
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/data/Thing.kt
@@ -0,0 +1,8 @@
+package com.makentoshe.androidgithubcitemplate.data
+
+data class Thing(
+    val title: String? = null,
+    val description: String? = null,
+    val thingId: String? = null,
+    val img: String? = null
+)
