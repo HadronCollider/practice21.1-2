46b4409fdc277beea5b061b528372f2811dac2f9
nofrog <ivansannikov29@gmail.com>
2021-06-07 22:35:15 +0300

    Commit has working scrolling activity and menu, sorry for only 1 commit

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.makentoshe.androidgithubcitemplate
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val switchActivityIntent = Intent(this, ScrollingActivity::class.java)
+        startActivity(switchActivityIntent)
     }
 }
