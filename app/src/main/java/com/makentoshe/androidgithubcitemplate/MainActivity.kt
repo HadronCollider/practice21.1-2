66499ccbf8276597f3c4429207daaf6b20fc7f60
SM-164738592 <mary@also.ru>
2021-06-08 01:25:28 +0300

    Open or close main popup menu

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -2,11 +2,35 @@ package com.makentoshe.androidgithubcitemplate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import androidx.annotation.MenuRes
+import androidx.appcompat.widget.PopupMenu
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_menu)
+
+        val button = findViewById<ImageButton>(R.id.btn_main_menu)
+        button.setOnClickListener { v: View ->
+            showMenu(v, R.menu.popup_menu)
+        }
     }
+}
+private fun showMenu(v: View, @MenuRes menuRes: Int) {
+    val popup = PopupMenu(v.context!!, v)
+    popup.menuInflater.inflate(menuRes, popup.menu)
+
+    popup.setOnMenuItemClickListener {
+        //Here's a respond to menu item click
+
+        true
+    }
+    popup.setOnDismissListener {
+        //Respond to popup being dismissed
+
+    }
+    popup.show()
 }
