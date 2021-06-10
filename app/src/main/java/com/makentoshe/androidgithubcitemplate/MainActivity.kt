3e87cf77fd2f7e2391690ced7d68947160eeae1a
Vlad <andreevvlad443@gmail.com>
2021-06-10 15:58:11 +0300

    Main activity for register (only for debug)

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.makentoshe.androidgithubcitemplate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnGoReg = findViewById<Button>(R.id.btnGoReg)
+        btnGoReg.setOnClickListener() {
+            val i = Intent(this, RegisterActivity::class.java)
+            startActivity(i)
+        }
     }
 }
