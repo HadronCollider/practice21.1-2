988a9405d91e1e97543c07ba570343bee84be2a3
SM-164738592 <mary@also.ru>
2021-06-10 12:56:30 +0300

    New data class (has toDo): ElementMainList.kt

---
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/ElementMainList.kt
@@ -0,0 +1,11 @@
+package com.makentoshe.androidgithubcitemplate
+
+data class ElementMainList (
+    var id: String,
+    var name: String,
+    var description: String,
+    var photo: String //toDo: change "String" to another data type
+    )
+
+
+//? add link to another item
