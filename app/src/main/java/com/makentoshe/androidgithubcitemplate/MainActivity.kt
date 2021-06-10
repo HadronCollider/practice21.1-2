e6a7785eadedaa50c873083ef48691565c4d68bb
SM-164738592 <mary@also.ru>
2021-06-10 18:23:52 +0300

    Changed 1 letter

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -17,16 +17,37 @@ import java.util.ArrayList
 import java.util.List
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ElementMainListAdapter.OnItemClickListener {
+
+    //LISTS
+    private lateinit var elementMainListAdapter: ElementMainListAdapter
+    private var alertDialog: AlertDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_menu)
 
+        //POPUP MENU
         val button = findViewById<ImageButton>(R.id.btn_main_menu)
         button.setOnClickListener { v: View ->
             showMenu(v, R.menu.popup_menu)
         }
+
+        //LISTS
+
+        initRecyclerView()
+
+        /*
+        addPersonButton.setOnClickListener{ //toDo: part of popup_menu.xml
+            personAdapter.addPerson(
+                id = "1", //toDo
+                name = "Name", //toDo
+                description = "No comments", //toDo
+                avatarUrl = "https://miro.medium.com/max/2400/0*nwOC-SxX0_fb3Xwc.jpg"//toDo
+            )
+
+        }
+        */
     }
 }
 
@@ -37,12 +58,12 @@ private fun showMenu(v: View, @MenuRes menuRes: Int) {
 
     popup.setOnMenuItemClickListener {
         //Here's a respond to menu item click
-
+        //toDo
         true
     }
     popup.setOnDismissListener {
         //Respond to popup being dismissed
-
+        //toDo
     }
     popup.show()
 }
