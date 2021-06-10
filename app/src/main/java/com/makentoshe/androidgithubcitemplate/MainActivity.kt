65f36f1b43f6cf961b11308ef9b1e03f98ccaf63
Vlad <andreevvlad443@gmail.com>
2021-06-10 16:04:48 +0300

    Main activity for upload (only for debug)

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.makentoshe.androidgithubcitemplate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnGoUpload = findViewById<Button>(R.id.btnGoUpload)
+        btnGoUpload.setOnClickListener() {
+            val i = Intent(this, UploadActivity::class.java)
+            startActivity(i)
+        }
     }
 }
