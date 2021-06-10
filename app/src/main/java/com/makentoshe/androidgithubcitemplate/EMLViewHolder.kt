f963b18250344f5e46dab4de65935370d31c4e9b
SM-164738592 <mary@also.ru>
2021-06-10 19:55:29 +0300

    EMLViewHolder.kt (ElementMainListViewHolder)

---
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/EMLViewHolder.kt
@@ -0,0 +1,32 @@
+package com.makentoshe.androidgithubcitemplate
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.items_main.view.*
+
+class EMLViewHolder {
+
+}
+/*
+    private val itemClickListener: ElementMainList.onItemClickListener): RecyclerView.ViewHolder(itemView){ //problem with RecyclerView.ViewHolder(itemView
+        //toDo
+        fun bind(item: ElementMainList){
+            with(itemView){
+                item.run {
+                    nameTextView.text = name
+                    descriptionTextView.text = description
+
+                    Glide.with(context).load(avatarUrl).into(photoImageView)
+
+                    deleteItemImageView.setOnClickListener{
+                        ItemClickListener.OnItemClicked(id = id)
+                    }
+                }
+            }
+        }
+    }
+}
+
+*/
+
+//EML = "ElementMainList"
